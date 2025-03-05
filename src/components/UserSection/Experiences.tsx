46cace0150c50784bc27080a98fdfad2d3edefcc
marcj19 <paulomjunior7@gmail.com>
2025-03-05 16:57:59 -0300

    inicializando o compenete de esperiencias

--- src/components/UserSection/Experiences.tsx
+++ src/components/UserSection/Experiences.tsx
@@ -1,8 +1,48 @@
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const Experiences = () => {
     return (
-        <>
-        </>
-    )
-}
+        <section className="experiencias">
+            <div className="conteiner-experiences">
+                <h1>Minhas experiências</h1>
+                <div className="input-container">
+                    <input type="text" name="experiencia" />
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button>
+                                <PlusCircle />
+                            </Button>
+                        </DialogTrigger>
 
-export default Experiences
+                        <DialogContent>
+                            <p>Hello World</p>
+                        </DialogContent>
+                    </Dialog>
+                </div>
+                <label><input type="checkbox" />Não possuo experiência</label>
+            </div>
+            <div className="conteiner-experiences">
+                <h1>Minhas certificações</h1>
+                <div className="input-container">
+                    <input type="text" name="experiencia" />
+                    <Button>
+                        <PlusCircle />
+                    </Button>
+                </div>
+            </div>
+            <div className="conteiner-experiences">
+                <h1>Minhas licenças</h1>
+                <div className="input-container">
+                    <input type="text" name="experiencia" />
+                    <Button>
+                        <PlusCircle />
+                    </Button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Experiences;

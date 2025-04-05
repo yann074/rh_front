ea9edfb3c51461b59354489f977c075f2b4d83b7
yann074 <yannsmith2907@gmail.com>
2025-04-05 11:30:58 -0300

    feat: Apply jobs

--- src/components/UserSection/Apply.tsx
+++ src/components/UserSection/Apply.tsx
@@ -3,8 +3,8 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 
 export default function Apply() {
-    const { id } = useParams();
     
+    const { id } = useParams();
     useEffect(() => {
         if (!id) {
             console.error("ID não encontrado!");
@@ -13,7 +13,7 @@ export default function Apply() {
 
         const token = localStorage.getItem('token');
 
-        axios.post(`http://127.0.0.1:8000/api/job/${id}`, { id_vaga: id }, {
+        axios.post(`http://127.0.0.1:8000/api/apply_opportunities/${id}`, { id_vaga: id }, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
@@ -25,8 +25,26 @@ export default function Apply() {
             console.error(error.response.data);
         });
         
+        fetchUserData();
     }, [id]);
+    
+    const fetchUserData = async () => {
+        try {
 
+            const userProfileResponse = await axios.get('http://127.0.0.1:8000/api/userprofile', {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+              }
+            });
+          
+            console.log('Usuário autenticado:', userProfileResponse.data);
+            console.log('Token usado:', localStorage.getItem('token'));
+      
+          } catch (error) {
+            console.error('Erro ao buscar dados do usuário:', error);
+          }
+        };
+    
     return (
         <div className="flex justify-center items-center h-screen">
             <h1 className="text-2xl font-semibold">Candidatura enviada com sucesso!</h1>
